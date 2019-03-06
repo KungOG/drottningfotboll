010909594e9477640432e732fbc0449de8035e86
Oscar Gustafsson <oscar.gustafsson2@medieinstitutet.se>
2019-03-06 09:46:20 +0100

    First commit! Made structure and next step will be add views and components.

--- src/main.js
+++ src/main.js
@@ -1,12 +1,12 @@
 import Vue from 'vue'
 import App from './App.vue'
 import router from './router'
-import store from './store'
+import index from './store'
 
 Vue.config.productionTip = false
 
 new Vue({
   router,
-  store,
+  index,
   render: h => h(App)
 }).$mount('#app')
