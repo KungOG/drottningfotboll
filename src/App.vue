cccbbaafdd49f94c3f41572d5c3d6d121ef5be93
Oscar Gustafsson <oscar.gustafsson2@medieinstitutet.se>
2019-03-27 14:15:47 +0100

    more structured work finished

--- src/App.vue
+++ src/App.vue
@@ -2,7 +2,13 @@
   <div id="app">
     <div id="nav">
       <router-link to="/">Home</router-link> |
-      <router-link to="/about">About</router-link>
+      <router-link to="/about">About</router-link>|
+      <router-link to="/stats">Stats</router-link>|
+      <router-link to="/admin">Admin</router-link> |
+      <router-link to="/players">Players</router-link>|
+      <router-link to="/makegames">Make games</router-link> |
+      <router-link to="/makegroups">Make groups</router-link>|
+      <router-link to="/schedules">Schedules</router-link>|
     </div>
     <router-view/>
   </div>
--- src/router.js
+++ src/router.js
@@ -14,6 +14,41 @@ export default new Router({
       path: '/about',
       name: 'about',
       component: () => import('./views/About.vue')
+    },
+    {
+      path: '/stats',
+      name: 'stats',
+      component: () => import('./views/Stats.vue')
+    },
+    {
+      path: '/admin',
+      name: 'admin',
+      component: () => import('./views/Admin/Admin.vue')
+    },
+    {
+      path: '/groups',
+      name: 'groups',
+      component: () => import('./views/Admin/Groups.vue')
+    },
+    {
+      path: '/makegames',
+      name: 'makegames',
+      component: () => import('./views/Admin/MakeGames.vue')
+    },
+    {
+      path: '/makegroups',
+      name: 'makegroups',
+      component: () => import('./views/Admin/MakeGroups.vue')
+    },
+    {
+      path: '/players',
+      name: 'players',
+      component: () => import('./views/Admin/Players.vue')
+    },
+    {
+      path: '/schedules',
+      name: 'schedules',
+      component: () => import('./views/Admin/Schedules.vue')
     }
   ]
 })
