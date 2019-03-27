3e12d566d74d11aa2324476ace5ba050d538f98d
Oscar Gustafsson <oscar.gustafsson2@medieinstitutet.se>
2019-03-27 13:53:32 +0100

    We made basic structure for our application after a meeting and talking what we need as components and as views

---
+++ src/components/Admin/ChooseGame.vue
@@ -0,0 +1,9 @@
+<template>
+    
+</template>
+
+<script>
+export default {
+    name : 'choosegame'
+}
+</script>
---
+++ src/components/Admin/ChooseTeam.vue
@@ -0,0 +1,13 @@
+<template>
+    
+</template>
+
+<script>
+export default {
+    name : 'chooseteam'
+}
+</script>
+
+<style>
+
+</style>
