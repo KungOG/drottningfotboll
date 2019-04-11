f16dd4785fb41a880dc63d4b352e6a579cbd05fd
Oscar Gustafsson <oscar.gustafsson2@medieinstitutet.se>
2019-04-11 14:56:57 +0200

    deleted facebook login

--- src/components/Login.vue
+++ src/components/Login.vue
@@ -25,26 +25,6 @@ export default {
             }).catch((err) => {
                 alert('Whops, something happend here..' + err.message)
             });   
-        }, 
-        facebookLogin () {
-            const provider = new firebase.auth.FacebookAuthProvider();
-
-            firebase.auth().signInWithPopup(provider).then(function(result) {
-            // This gives you a Facebook Access Token. You can use it to access the Facebook API.
-            var token = result.credential.accessToken;
-            // The signed-in user info.
-            var user = result.user;
-            // ...
-            }).catch(function(error) {
-            // Handle Errors here.
-            var errorCode = error.code;
-            var errorMessage = error.message;
-            // The email of the user's account used.
-            var email = error.email;
-            // The firebase.auth.AuthCredential type that was used.
-            var credential = error.credential;
-            // ...
-            });
         }   
     }
 }
