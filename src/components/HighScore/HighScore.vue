6cc9cd492b5ccf7d1de5cd40ed15e21e81cfe2b8
Oscar Gustafsson <oscar-gustafsson@live.com>
2019-07-05 23:01:36 +0200

    highscore grid

--- src/components/HighScore/HighScore.vue
+++ src/components/HighScore/HighScore.vue
@@ -1,7 +1,7 @@
 <template>
     <main class="highscore-page">
       <section class="highscore-header">
-        <select>
+        <select v-model="selected" @change="setOption">
           <option>Poäng</option>
           <option>Vinst</option>
           <option>Lika</option>
@@ -10,19 +10,32 @@
         </select>
       </section>
       <article class="list-wrapper" v-for="(item, index) in teamPlayers.slice().reverse()" :key="index">
-        <section class="container">
-          <section class="name">
-            <h3>{{item.name}}</h3>
-          </section>
           <section class="score">
-            <p>{{item.win}}</p>
-            <p>{{item.loss}}</p>
-            <p>{{item.loss}}</p>
+            <div class="name">
+              <h3>{{item.name}}</h3>
+            </div>
+            <div class='highlight'>
+              <span>{{item.point}}</span><br>
+              <span>Poäng </span>
+            </div>
+            <div class="win">
+              <span>Vinster </span>
+              <span>{{item.win}}</span>
+            </div>
+            <div class="lost">
+              <span>Förluster </span>
+              <span>{{item.loss}}</span>
+            </div>
+            <br>
+            <div class="tie">
+              <span>Oavgjort </span>
+              <span>{{item.tie}}</span>
+            </div>
+            <div class="goal">
+              <span>Mål </span>
+              <span>{{item.goal}}</span>
+            </div>
           </section>
-          <aside id="date">
-            {{item.point}}
-          </aside>
-        </section>
       </article>
     </main>
 </template>
@@ -32,52 +45,21 @@ import db from '@/firebaseInit'
 
 export default {
     name : 'highscore',
+    data() {
+      return {
+        selected: '',
+      }
+    },
     computed: {
       teamPlayers() {
         return this.$store.state.teamPlayers;
+      }
+    },
+    methods: {
+      setOption () {
+        console.log(this.selected)
       }
     }
   }
 
 </script>
-
-<style lang="scss">
-
-.main {
-  border: 1px solid black;
-  border-radius: 5px;
-
-  .list-wrapper {
-    display: flex;
-    border-bottom: 1px solid gray;
-    padding: .5rem;
-    .container {
-      display: flex;
-      flex-direction: column;
-      justify-content: center;
-      flex: 1;
-      h3 {
-        text-transform: uppercase;
-        margin: 0;
-        font-size: 1.2rem;
-      }
-      .score {
-        display: flex;
-        flex-direction: row;
-        #win {
-          margin-right: 1.5rem;
-        }
-      }
-    }
-    #date {
-      box-sizing: border-box;
-      padding: 10px 5px 15px 5px;
-      margin-right: 2rem;
-      text-align: center;
-      font-size: 3rem;
-      width: 70px;
-      height: 60px;
-    }
-  }
-}
-</style>
